719b0947dfa12dfe1c149dd371a066382e8362b2
Chiji2 <nwosuchijioke@gmail.com>
2025-02-24 08:38:10 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
-if st.button("Test this one"):
+if st.button("Test this one_now I know why it failed before"):
   time_series = np.random.randn(100)
   plt.plot(time_series)
   plt.title("Random 100-unit Time Series")
